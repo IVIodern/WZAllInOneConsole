e4c507a21ecab1ccf702db15b31d10eec696ab3e
Vextor <vexter@outlook.com>
2013-04-24 17:38:41 +0100

    Finishing up
    Added a commit which basically adds in all the functions, plus when
    you've done a certain function, it returns back to the starting area of
    functions - easier to explain by actually using it.

--- WZ All in one console/Program.cs
+++ WZ All in one console/Program.cs
@@ -14,132 +14,151 @@ namespace WZ_All_in_one_console
     {
         static void Main(string[] args)
         {
-            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-            // string rcon = ""; ---- i failed here, previously it was Console.ReadLine = rcon below, but since it was the wrong syntax it didn't need to use the string
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("What Application would you like to open?");
-            Console.WriteLine("The variables are");
-            Console.WriteLine("1) WZ RCON Client");
-            Console.WriteLine("2) Demo Uploader");
-            Console.WriteLine("3) Remote Desktop");
+            int i = 1;
 
-            string readline = Console.ReadLine(); 
-            if (readline == "1")
+            while (i == 1)
             {
-                // tries to launch the program, does catch if it can't
-                try
+                string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                // string rcon = ""; ---- i failed here, previously it was Console.ReadLine = rcon below, but since it was the wrong syntax it didn't need to use the string
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("=================================================");
+                Console.WriteLine("What Application would you like to open?");
+                Console.WriteLine("The variables are");
+                Console.WriteLine("1) WZ RCON Client");
+                Console.WriteLine("2) Demo Uploader");
+                Console.WriteLine("3) Remote Desktop");
+                Console.WriteLine("4) Close the Application");
+                Console.WriteLine("=================================================");
+
+                string readline = Console.ReadLine();
+                if (readline == "1")
                 {
-                    Process.Start(@"wzsoftware\wz_rcon_v2.1.exe");
-                    Console.WriteLine("Use your WarZone Gaming credentials to logon.");
-                    Console.ReadKey(); 
+                    // tries to launch the program, does catch if it can't
+                    try
+                    {
+                        Process.Start(@"wzsoftware\wz_rcon_v2.1.exe");
+                        Console.WriteLine("Use your WarZone Gaming credentials to logon. Hit a Key to continue.");
+                        
+                        continue;
+                    }
+
+                    catch
+                    {
+                        // pointing the obvious
+                        Console.WriteLine("You don't have the RCON client installed.");
+
+                        Directory.CreateDirectory(@"wzsoftware\");
+
+                        Console.WriteLine("Downloading the RCON tool...");
+
+                        WebClient webClient = new WebClient();
+
+
+                        webClient.DownloadFile("http://warzone-gaming.co.uk/downloads/wz_rcon_v2.1.exe", @"wzsoftware\wz_rcon_v2.1.exe");
+
+                        Console.WriteLine("Download Complete, launching now.");
+
+
+                        // once it's downloaded, launch it 
+
+                        Process.Start(@"wzsoftware\wz_rcon_v2.1.exe");
+                        Console.WriteLine("Use your WarZone Gaming credentials to logon.");
+                        
+                        continue;
+
+
+
+
+
+                    }
+
+
+
                 }
 
-                catch
+                if (readline == "2")
                 {
-                    // pointing the obvious
-                    Console.WriteLine("You don't have the RCON client installed.");
+                    // tries to launch the program, does catch if it can't
+                    try
+                    {
+                        Process.Start(@"wzsoftware\emoUploader2.exe");
+                        Console.WriteLine("Use your WarZone Gaming credentials to logon.");
+                        
+                        continue;
+                    }
 
-                    Directory.CreateDirectory(@"wzsoftware\");
+                    catch
+                    {
+                        // pointing the obvious
+                        Console.WriteLine("You don't have the Demo Uploader client installed.");
 
-                    Console.WriteLine("Downloading the RCON tool...");
+                        Directory.CreateDirectory(@"wzsoftware\");
 
-                    WebClient webClient = new WebClient();
+                        Console.WriteLine("Downloading the Demo Uploader tool...");
 
-
-                    webClient.DownloadFile("http://warzone-gaming.co.uk/downloads/wz_rcon_v2.1.exe", @"wzsoftware\wz_rcon_v2.1.exe");
-                   
-                    Console.WriteLine("Download Complete, launching now.");
-                    
-                   
-                    // once it's downloaded, launch it 
-                    
-                    Process.Start(@"wzsoftware\wz_rcon_v2.1.exe");
-                    Console.WriteLine("Use your WarZone Gaming credentials to logon.");
-                    Console.ReadKey(); 
-                
-                    
-               
-                    
- 
-                } 
+                        WebClient webClient = new WebClient();
 
 
-               
-            }
+                        webClient.DownloadFile("http://warzone-gaming.co.uk/downloads/DemoUploader2.exe", @"wzsoftware\DemoUploader2.exe");
 
-            if (readline == "2")
-            {
-                // tries to launch the program, does catch if it can't
-                try
+                        Console.WriteLine("Download Complete, launching now.");
+
+
+                        // once it's downloaded, launch it 
+
+                        Process.Start(@"wzsoftware\DemoUploader2.exe");
+                        Console.WriteLine("Use your WarZone Gaming credentials to logon.");
+                      
+                        continue;
+
+
+
+
+
+                    }
+
+
+
+                }
+
+                if (readline == "3")
                 {
-                    Process.Start(@"wzsoftware\emoUploader2.exe");
-                    Console.WriteLine("Use your WarZone Gaming credentials to logon.");
+
+                    try
+                    {
+                        Process.Start("mstsc.exe");
+                        Console.WriteLine("Remote Desktop Launched, now launching the thread with logon details.");
+                        Process.Start("http://warzone-gaming.co.uk/forums/index.php?/topic/4715-rcon-firewall-access-v20/");
+                       
+                        continue;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Outdated operating system. Must be Vista or higher.");
+                       
+                        continue;
+                    }
+                }
+
+
+                if (readline == "4")
+                {
+                    Console.WriteLine("Exiting Application. Click any key to continue.");
                     Console.ReadKey();
+                    Environment.Exit(0);
                 }
-
-                catch
+                else
                 {
-                    // pointing the obvious
-                    Console.WriteLine("You don't have the Demo Uploader client installed.");
+                    Console.WriteLine("Only choose 1, 2, 3 or 4!");
 
-                    Directory.CreateDirectory(@"wzsoftware\");
-
-                    Console.WriteLine("Downloading the Demo Uploader tool...");
-
-                    WebClient webClient = new WebClient();
-
-
-                    webClient.DownloadFile("http://warzone-gaming.co.uk/downloads/DemoUploader2.exe", @"wzsoftware\DemoUploader2.exe");
-
-                    Console.WriteLine("Download Complete, launching now.");
-
-
-                    // once it's downloaded, launch it 
-
-                    Process.Start(@"wzsoftware\DemoUploader2.exe");
-                    Console.WriteLine("Use your WarZone Gaming credentials to logon.");
-                    Console.ReadKey();
-
-
-
-
+                    continue;
 
                 }
-
 
 
             }
 
-            if (readline == "3")
-            {
-
-                try
-                {
-                    Process.Start("mstsc.exe");
-                    Console.WriteLine("Remote Desktop Launched, now launching the thread with logon details.");
-                    Process.Start("http://warzone-gaming.co.uk/forums/index.php?/topic/4715-rcon-firewall-access-v20/");
-                    Console.ReadKey(); 
-
-                }
-                catch
-                {
-                    Console.WriteLine("Outdated operating system. Must be Vista or higher.");
-                    Console.ReadKey(); 
-                }
-            } 
-
-
-            else
-            {
-                Console.WriteLine("Only choose 1 or 2! Relaunch the client!");
-                Console.ReadKey();
-
-            }
-           
-       
         }
-
-     
 
 
     }
